05ccabcb992950287b06c1d30afef091ed9ed953
unknown <nksayyad2900.com>
2020-12-04 11:58:46 +0530

    [NIJAM] Add:Welcome To AddressBook.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+namespace AddressBookApp
+{
+    class Program
+    {
+        public static LinkedList<UserInfo> userList = new LinkedList<UserInfo>();
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("******WELCOME TO ADDRESS BOOK******");
+        }      	
+    }
+}
