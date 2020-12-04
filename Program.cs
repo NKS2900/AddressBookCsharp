abd89edeb19636052646441ffb5b37078d6cbb47
unknown <nksayyad2900.com>
2020-12-04 12:55:21 +0530

    [NIJAM] Add:Creating Contact in AddressBook.

--- Program.cs
+++ Program.cs
@@ -6,9 +6,29 @@ namespace AddressBookApp
     {
         public static LinkedList<UserInfo> userList = new LinkedList<UserInfo>();
 
-        public static void Main(string[] args)
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("******WELCOME TO ADDRESS BOOK******");
+			addUser();			
+		}
+        public static void addUser()
         {
-            Console.WriteLine("******WELCOME TO ADDRESS BOOK******");
-        }      	
+            Console.WriteLine("Enter FirstName: ");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter LastName: ");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Enter Address : ");
+            string address = Console.ReadLine();
+            Console.WriteLine("Enter Contact No: ");
+            string contact = Console.ReadLine();
+            Console.WriteLine("Enter State : ");
+            string state = Console.ReadLine();
+            Console.WriteLine("Enter zip : ");
+            string zip = Console.ReadLine();
+            UserInfo user = new UserInfo(firstName, lastName, address, contact, state, zip);
+            userList.AddFirst(user);
+            user.print();
+            Console.WriteLine("Details Added Successfully. \n");
+        }
     }
 }
---
+++ UserInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookApp
+{
+	public class UserInfo
+	{		
+		String firstName;
+		String lastName;
+		String address;
+		String contact;
+		String state;
+		String zip;
+
+		public UserInfo(String firstName, String lastName, String address, String contact, String state, String zip)
+		{
+			this.firstName = firstName;
+			this.lastName = lastName;
+			this.address = address;
+			this.contact = contact;
+			this.state = state;
+			this.zip = zip;
+		}
+		public void print()
+		{
+
+			Console.WriteLine(firstName + " \t  " + lastName +" \t  " + address + " \t  " + state + " \t   " +
+								contact + " \t " + zip); ;
+		}
+	}
+}
